36e38f483872d8d9f6351115e4c801bb86b7af01
Abood K. AMA <abdamapro22@gmail.com>
2023-02-18 22:10:24 +0200

    done 9.2 , 9.4 , 9.6

--- src/Chapters/Chapter09/P9_4.java
+++ src/Chapters/Chapter09/P9_4.java
@@ -1,4 +1,20 @@
 package Chapters.Chapter09;
 
+import java.util.Random;
+
 public class P9_4 {
+
+    public static void main(String[] args) {
+
+        Random random = new Random(1000);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(random.nextInt(100) + " ");
+
+            }
+            System.out.println();
+        }
+
+    }
+
 }
--- src/Chapters/Chapter09/P9_6.java
+++ src/Chapters/Chapter09/P9_6.java
@@ -1,4 +1,54 @@
 package Chapters.Chapter09;
-
+import java.util.*;
 public class P9_6 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        StopWatch stopWatch = new StopWatch();
+
+        int [] x = new int[100000];
+
+        for (int i = 0; i < x.length; i++) {
+            x[i] = random.nextInt(100000);
+        }
+        stopWatch.start();
+        selectionSort(x);
+        stopWatch.stop();
+        System.out.print("Elapsed time is: " + stopWatch.getElapsedTime() + " ms");
+    }
+    public static void selectionSort(int[] nums) {
+        for (int n = 0; n < nums.length - 1; n++) {
+            int min = nums[n];
+            int minIdx = n;
+            for (int k = n + 1; k < nums.length; k++) {
+                if (nums[k] < min) {
+                    min = nums[k];
+                    minIdx = k;
+                }}
+            // Swap
+            if (minIdx != n) {
+                nums[minIdx] = nums[n];
+                nums[n] = min;
+            }}}
+}
+class StopWatch {
+    private long startTime;
+    private long endTime;
+    public long getStartTime() {
+        return startTime;
+    }
+    public long getEndTime() {
+        return endTime;
+    }
+    StopWatch() {
+        startTime = System.currentTimeMillis();
+    }
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
 }
