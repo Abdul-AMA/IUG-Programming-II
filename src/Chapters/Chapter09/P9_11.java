6bcc4c291a94fe8536d390794762a132ea435dd9
Abood K. AMA <abdamapro22@gmail.com>
2023-02-19 01:02:11 +0200

    ass 1 done

--- src/Chapters/Chapter09/P9_11.java
+++ src/Chapters/Chapter09/P9_11.java
@@ -1,4 +1,33 @@
 package Chapters.Chapter09;
-
 public class P9_11 {
+    public static void main(String[] args) {
+        LinearEquation linearEquation = new LinearEquation(6, 2, 3, 1, 3, 2);
+        System.out.println(linearEquation.isSolvable() ? "x = " + linearEquation.getX() +
+                ", Y = " + linearEquation.getY() : "The equation has no solution");
+    }
+}
+class LinearEquation {
+    int a, b, c, d, e, f;
+    LinearEquation(int a, int b, int c, int d, int e, int f) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+    }
+    public int getA() {return a;}
+    public int getB() {return b;}
+    public int getC() {return c;}
+    public int getD() {return d;}
+    public int getE() {return e;}
+    public int getF() {return f;}
+    public boolean isSolvable() {
+        if ((a * d) - (b * c) == 0) {
+            return false;
+        } else
+            return true;
+    }
+    public int getX() {return ((e * d) - (b * f)) / ((a * d) - (b * c));}
+    public int getY() {return ((a * f) - (e * c)) / ((a * d) - (b * c));}
 }
--- src/Chapters/Chapter09/P9_7.java
+++ src/Chapters/Chapter09/P9_7.java
@@ -1,4 +1,58 @@
 package Chapters.Chapter09;
-
+import java.util.*;
 public class P9_7 {
+    public static void main(String[] args) {
+        Account account = new Account(1122, 20000, 4.5);
+        account.withdraw(2500);
+        account.deposit(3000);
+        System.out.println("balance = " + account.getBalance());
+        System.out.println("monthly interest = " + account.getMonthlyInterest());
+        System.out.println("account was created on: " + account.getDateCreated());
+    }
 }
+class Account {
+    private int id = 0;
+    private double balance = 0;
+    private double annualInterestRate = 0;
+    private Date dateCreated;
+    Account() {
+        dateCreated = new Date();
+    }
+    Account(int id, double balance, double annualInterestRate) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+        dateCreated = new Date();
+    }
+    public int getId() {
+        return id;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+    public double getMonthlyInterest() {
+        return ((annualInterestRate / 100) / 12.0) * balance;
+    }
+    public void withdraw(double num) {
+        balance -= num;
+    }
+    public void deposit(double num) {
+        balance += num;
+    }
+}
+
