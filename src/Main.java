9f4210e56d6a4b625efe5bf0697a0473c40c13f4
Abood K. AMA <abdamapro22@gmail.com>
2023-03-05 00:18:57 +0200

    pre lab 4 done

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,57 @@ import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
+        Shape2 shape2 = new Shape2();
+        Shape shape = new Shape();
+        Circle cir = new Circle();
+        Rectangle rect = new Rectangle();
 
-        String x1 = "a ";
-        String x2 = "aa";
-
-        System.out.println((int) (' ' - 'a'));
-        System.out.print(x1.compareTo(x2));
-
-
+        System.out.println(show(shape2)); // No value
+        System.out.println(show(shape)); // No value
+        System.out.println(show(cir)); // No value
+        System.out.println(show(rect)); // No value
 
     }
 
+    public static String show(Object obj) {
+        if (obj instanceof Shape) {
+            return ((Shape) obj).print();
+        }
+        return "No value";
+    }
+}
+
+
+class Shape {
+    public Shape() {
+    }
+
+    public String print() {
+        return "General";
+    }
+}
+
+class Shape2 {
+    public Shape2() {
+    }
+}
+
+class Circle extends Shape {
+    public Circle() {
+    }
+
+    @Override
+    public String print() {
+        return "Circle";
+    }
+}
+
+class Rectangle extends Shape {
+    public Rectangle() {
+    }
+
+    @Override
+    public String print() {
+        return "Rectangle";
+    }
 }
